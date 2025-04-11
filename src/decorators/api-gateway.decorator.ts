fffd3cc42304b0cac95a2c1e0350e7c94de66225
ricoyduqs <ricardo.pena@yduqs.com.br>
2025-04-11 09:24:55 -0300

    ajuste de uri

--- src/decorators/api-gateway.decorator.ts
+++ src/decorators/api-gateway.decorator.ts
@@ -4,7 +4,7 @@ export const ApiGatewayIntegration = (method: string, path: string) => {
   return ApiExtension('x-amazon-apigateway-integration', {
     type: 'HTTP_PROXY',
     httpMethod: method,
-    uri: '${URI}' + path,
+    uri: process.env.URI + path,
     payloadFormatVersion: 1.0,
   });
 };
